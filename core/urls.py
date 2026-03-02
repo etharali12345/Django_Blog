9c580b6b0d7d74449182580170a86a79e3c14db0
etharali12345 <mayadaethar@gmail.com>
2026-03-02 08:59:41 +0300

    "update"

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
-    path('api/posts/', views.api_posts, name='api_posts'),
     path('profile/', views.profile, name='profile'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
